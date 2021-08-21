e3d2bc28faac09168a90d6510e32dae0b17c553f
Nirav Koley <niravkoley@gmail.com>
2021-08-21 15:33:42 -0500

    Got the basics to work

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import StyleSelector from "./components/StyleSelector";
 import { BrowserRouter as Router } from 'react-router-dom';
 import Title from "./components/main/Title";
-import WeekView from "./components/main/WeekView";
+import Day from "./components/main/Day";
+import dayjs from 'dayjs'
 
 function App() {
   const [activeStyle,  setActiveStyle] = useState('turtle') // This is the active state, you need to pass it into any components you make 
-  const [everything, setEverything] = useState([[[0,0,0,0,0,0,0]]])
+  const [activeDate, setActiveDate] = useState(dayjs(new Date()))
+  const [totalMood, setTotalMood] = useState({})
+  const [store, setStore] = useState(false)
+  const mounted = useRef(false);
+
+  useEffect(()=>{
+    if(localStorage.getItem('moodCalendar') !== null){
+      console.log("Load")
+      setTotalMood(JSON.parse(localStorage.getItem('moodCalendar')))
+    }
+    if(localStorage.getItem('date') !== null){
+      console.log("Load")
+      setActiveDate(dayjs(JSON.parse(localStorage.getItem('date'))))
+    }
+  }, []) 
+    
+  useEffect(()=>{
+    if(mounted.current !== false){
+      localStorage.setItem('moodCalendar', JSON.stringify(totalMood))
+      localStorage.setItem('date', JSON.stringify(activeDate))
+    } else{
+      mounted.current = true;
+    }
+  },[store])
 
   return (
     <Router>
       <StyleSelector setActiveStyle = {setActiveStyle} /> {/* The active state wont affect the style bar */}
       <div>
         <Title activeStyle={activeStyle}/>
-        <WeekView activeStyle={activeStyle}/>
+        <Day activeDate={activeDate} setActiveDate={setActiveDate} totalMood={totalMood} setTotalMood={setTotalMood} store={store} setStore={setStore}/>
       </div>      
     </Router>
   );
---
+++ src/components/main/Day.js
@@ -0,0 +1,79 @@
+import React, {useState} from 'react'
+import dayjs from 'dayjs'
+import DayOfYear from 'dayjs/plugin/dayOfYear'
+
+dayjs.extend(DayOfYear);
+
+const Day = ({ activeDate, setActiveDate, totalMood, setTotalMood, store, setStore}) => {
+    let statusList = ['none','happy','medium','sad']
+    const [status, setStatus] = useState(0)
+    const [saved, setSaved] = useState(false)
+    let day = activeDate.dayOfYear()
+
+    
+
+    return (
+        <>
+            <div className='button-center'>
+                {activeDate.format('dddd, MMMM D')}
+            </div>
+            <div className="week-box">
+                <div 
+                className={'day active '+statusList[status] + ' '+ (saved?'saved':'')} 
+                onClick={
+                    ()=>{
+                        
+                        if(status===(statusList.length-1)){
+                            setStatus(0)
+                        } else{
+                            setStatus(status+1)
+                        }                  
+                    }
+                }
+                >
+                </div>
+            </div>
+            <button 
+                className='button-center' 
+                onClick={()=>{
+                    setTotalMood({...totalMood, [activeDate.dayOfYear()]:status})
+                    setActiveDate(activeDate.add(1,'day'))
+                    setStatus(0)
+                    setSaved(0)
+                    setStore(!store)
+                }}
+            > Submit mood</button> {/* ive had it with this project, ive spent too long fix this button yourself probably bootstrap it please*/}
+            <button 
+                className='button-center'
+                onClick={()=>{
+                    setActiveDate(activeDate.add(1,'day'))
+                    if(totalMood[day] !== undefined){
+                        console.log("here")
+                        setStatus(totalMood[day])
+                        setSaved(true)
+                    } else{
+                        setStatus(0)
+                        setSaved(0)
+                    }
+                }}
+            >Next</button>
+
+            <button 
+                className='button-center'
+                onClick={()=>{
+                    setActiveDate(activeDate.subtract(1,'day'))
+                    if(totalMood[day] !== undefined){
+                        console.log("here")
+                        setStatus(totalMood[day])
+                        setSaved(true)
+                    } else{
+                        setStatus(0)
+                        setSaved(0)
+                    }
+                }}
+            >Prev</button>
+        </>
+    )
+}
+
+export default Day
